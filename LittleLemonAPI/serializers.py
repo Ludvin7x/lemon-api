fda966c3ead740f28436db6d219ea28d00b2b1e8
Ludvin F. <ludwing.df@live.com>
2025-04-21 02:09:53 -0600

    1.Serializers 2.View /api/menu-Item 3.urls.py (project and app)

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,3 +1,12 @@
 from rest_framework import serializers
 from .models import MenuItem, Category, Cart, Order, OrderItem
 
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+from django.contrib.auth.models import Group
 
-# Create your views here.
+class IsManager(permissions.BasePermission):
+    """
+    Custom permission to only allow managers to access the view.
+    """
+    def has_permission(self, request, view):
+        # Check if the user is authenticated and belongs to the 'Manager' group
+        return request.user.is_authenticated and request.user.groups.filter(name='Manager').exists()
+    
+class MenuItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows menu items to be viewed or edited.
+    """
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+   
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            self.permission_classes = [IsAuthenticated, IsManager]
+        else:
+            self.permission_classes = [permissions.AllowAny]
+        return super().get_permissions()
