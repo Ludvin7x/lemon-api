027f0f310e3a8d134fc7c84e8dc71bd9d0f25bd1
Ludvin F. <ludwing.df@live.com>
2025-05-01 19:26:16 -0600

    Implement initial data loading on app ready; add user and group creation, and seed categories and menu items

--- LittleLemonAPI/apps.py
+++ LittleLemonAPI/apps.py
@@ -1,10 +1,10 @@
 # LittleLemonAPI/apps.py
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 class LittlelemonapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'LittleLemonAPI'
 
-    def ready(self):
-        import LittleLemonAPI.signals
+def ready(self):
+        from .initial_data import load_initial_data
+        load_initial_data()
