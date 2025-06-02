c8fda7b16f3c6b8f63bcb24d6f6a0aff63a775dd
Ludvin F. <ludwing.df@live.com>
2025-06-01 21:15:24 -0600

    Add descriptions to MenuItem and update initial data creation logic

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -15,6 +15,7 @@ class Category(models.Model):
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True)
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
     featured = models.BooleanField(default=False, db_index=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="menu_items")
--- LittleLemonAPI/serializers/menuitem_serializers.py
+++ LittleLemonAPI/serializers/menuitem_serializers.py
@@ -7,4 +7,4 @@ class MenuItemSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'price', 'featured', 'category']
+        fields = '__all__'
